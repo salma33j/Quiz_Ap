84b971ce160469b1263dacc487268434e8241639
Aya <ayanakhla2005@gmail.com>
2026-04-23 22:49:53 +0000

    J'ai ajouter des dependances dans pom.xml et j'ai ajouter le code dans quelques classes

--- src/main/java/com/exemple/quiz_app/auth/model/Role.java
+++ src/main/java/com/exemple/quiz_app/auth/model/Role.java
@@ -1,4 +1,27 @@
 package com.exemple.quiz_app.auth.model;
-
-public class Role {
+public enum Role {
+      ETUDIANT,
+      ENSEIGNANT;
+    // Méthode utilitaire pour vérifier le rôle
+    public boolean isEtudiant() {
+        return this == ETUDIANT;
+    }
+    public boolean isEnseignant() {
+        return this == ENSEIGNANT;
+    }
+    // Convertir une String en Role (utile pour les requêtes)
+    public static Role fromString(String role) {
+        try {
+            return Role.valueOf(role.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            return ETUDIANT; // Valeur par défaut
+        }
+    }
 }
+
+
+
+
+
+
+
