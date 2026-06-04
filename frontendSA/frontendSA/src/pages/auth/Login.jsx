62a0abf4be7982d62951e420f7d992845210792a
akils <akilsalma2005@gmail.com>
2026-06-04 17:42:12 +0100

    finale

--- frontendSA/frontendSA/src/pages/auth/Login.jsx
+++ frontendSA/frontendSA/src/pages/auth/Login.jsx
@@ -75,7 +75,7 @@ export default function Login() {
             <h1>Bienvenue</h1>
 
             <p className={styles.subtitle}>
-              Connectez-vous à votre plateforme Quiz FSB
+              Connectez-vous à votre plateforme Quiz APP
             </p>
 
             {error && <div className={styles.error}>{error}</div>}
