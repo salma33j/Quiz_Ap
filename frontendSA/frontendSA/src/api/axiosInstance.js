6141cf26cbe47267eebf26a4536284b405f316e9
akils <akilsalma2005@gmail.com>
2026-06-06 02:20:01 +0100

    finaleF

--- frontendSA/frontendSA/src/api/axiosInstance.js
+++ frontendSA/frontendSA/src/api/axiosInstance.js
@@ -10,8 +10,6 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
-
-    // ✅ Ne pas envoyer token expiré/ancien vers login
     const isLoginRequest = config.url?.includes("/auth/login");
 
     if (token && !isLoginRequest) {
@@ -26,8 +24,7 @@ axiosInstance.interceptors.request.use(
 axiosInstance.interceptors.response.use(
   (response) => response,
   (error) => {
-    // ✅ Si token expiré ou accès refusé
-    if (error.response?.status === 401 || error.response?.status === 403) {
+    if (error.response?.status === 401) {
       localStorage.removeItem("token");
       localStorage.removeItem("user");
 
